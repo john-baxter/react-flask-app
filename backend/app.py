805c3d1e66d81947959470892d2f41f840f575bb
john-baxter <john.g.baxter@googlemail.com>
2022-02-04 10:39:21 +0000

    Create file backend/app.py and add import statements and application instances.

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+ma = Marshmallow()
+cors = CORS()
+
+
